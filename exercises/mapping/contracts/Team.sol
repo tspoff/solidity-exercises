1105ad79294bfa5f88d3fba7460812bf42b9c901
tspoff <tspoff@gmail.com>
2018-05-21 13:02:20 -0500

    Mapping exercise

--- exercises/mapping/contracts/Team.sol
+++ exercises/mapping/contracts/Team.sol
@@ -6,10 +6,20 @@ contract Team {
     }
 
     // create public mapping that maps member id (uint) to a Member
+    mapping (uint => Member) public members;
 
     // create setMember for adding a new member that accepts _id, and _name as arguments
+    function setMember(uint _id, string _name) public{
+        members[_id].name = _name;
+    }
 
     // create getMemberName that returns name of the member by id
+    function getMemberName(uint _id) public view returns (string) {
+        return members[_id].name;
+    }
 
     // create removeMember that "removes" member by id
+    function removeMember(uint _id) public {
+        members[_id].name = "";
+    }
 }
