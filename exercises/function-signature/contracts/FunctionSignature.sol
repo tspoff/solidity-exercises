dd33216e1bc7f2907c64b70f841df1fa606c365a
tspoff <tspoff@gmail.com>
2018-05-23 11:13:03 -0500

    function-signature

--- exercises/function-signature/contracts/FunctionSignature.sol
+++ exercises/function-signature/contracts/FunctionSignature.sol
@@ -9,5 +9,7 @@ contract FunctionSignature {
     function isMoreThanFive(uint _value) public returns (bool) {
         // return true if calling moreThanFive succeeded and false if it failed
         // create function signature and pass _value as an argument
+        return address(this).call(bytes4(keccak256("moreThanFive(uint256)")), _value);
+
     }
 }
